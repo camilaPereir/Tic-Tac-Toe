3d0c6ed18570af34a720eb9018f102bc40ec2d0b
camilaPereir <camila379.cp@gmail.com>
2021-07-04 21:41:05 -0300

    deploy with vercel

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deploy
+          Deploy com Vercel
         </a>
       </header>
     </div>
