44641a585d4bc19fb86b34f50c412699c2439769
Camila Pereira <camila379.cp@gmail.com>
2021-07-10 23:01:58 -0300

    add logic of game

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { FC, useEffect, useState } from "react";
 import styled from "styled-components";
-import { CellValue } from "./Cell";
+import { Cell, CellValue } from "./Cell";
 
 const BoardWrapper = styled.div`
   background-color: #999999;
@@ -13,19 +14,52 @@ const BoardWrapper = styled.div`
   row-gap: 6px;
 `;
 
-type Winner = CellValue | "tie";
+export type Winner = CellValue | "tie";
 
 type BoardProps = {
   onGameEnd(winner: Winner): void;
 };
 
+// prettier-ignore
+const winnerConditions = [
+  [0,1,2], [3,4,5], [6,7,8], //Horizontal
+  [0,3,6], [1,4,7], [2,5,8], //Vertical
+  [0,4,8], [2,4,6] //Diagonal
+]
+
 export const Board: FC<BoardProps> = ({ onGameEnd }) => {
   const [cells, setCells] = useState<CellValue[]>(Array(9).fill(undefined));
+
+  const currentShape: CellValue = cells.filter(Boolean).length % 2 ? "o" : "x";
+
+  const winningCondition = winnerConditions.find((condition) => {
+    const line = condition.map((cellIndex) => cells[cellIndex]);
+    return line[0] && line.every((cellValue) => cellValue === line[0]);
+  });
+
+  const tie = cells.filter(Boolean).length === 9;
+
+  const winningShape = winningCondition
+    ? cells[winningCondition[0]]
+    : undefined;
+
+  const toggleCell = (index: number) => {
+    setCells((cells) => cells.map((c, i) => (i === index ? currentShape : c)));
+  };
+
+  useEffect(() => {
+    if (winningShape) {
+      return onGameEnd(winningShape);
+    }
+    if (tie) {
+      return onGameEnd("tie");
+    }
+  }, [winningShape, tie, onGameEnd]);
 
   return (
     <BoardWrapper>
       {cells.map((cell, i) => (
-        <Cell key={i} />
+        <Cell key={i} value={cell} index={i} toggle={toggleCell} />
       ))}
     </BoardWrapper>
   );
--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,15 +1,26 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import styled from "styled-components";
 
-export type CellValue = "x" | "O" | undefined;
+export type CellValue = "x" | "o" | undefined;
+
+const CellWrapper = styled.button`
+  background-color: #ffffff;
+  margin-bottom: -1px;
+  border: none;
+  cursor: pointer;
+  outline: none;
+`;
 
 type CellProps = {
   value: CellValue;
-  toggle: index(number)
+  toggle(index: number): void;
+  index: number;
 };
 
-const Cell = styled.div`
-  background-color: #ffffff;
-`;
-
-export const Cell: FC = () => {};
+export const Cell: FC<CellProps> = ({ value, toggle, index }) => {
+  return (
+    <CellWrapper onClick={() => toggle(index)}>
+      {value === "o" ? "O" : value ? "X" : null}
+    </CellWrapper>
+  );
+};
